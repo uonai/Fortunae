5d1afdc2d29383af1a95bf812799d14cd663c3e3
Uonai <cs.mccaleb@gmail.com>
2020-05-10 13:17:25 -0500

    add history save

--- client/js/store.js
+++ client/js/store.js
@@ -31,9 +31,27 @@ export default class Store {
   static saveJSON() {
     const fs = require("fs");
 
+    // save main
     const fileName = Helper.generateUUIDv4();
     const items = JSON.stringify(localStorage.getItem("items"));
     fs.writeFile(__dirname + "/db/" + fileName, items, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      alert("file saved");
+    });
+
+    this.saveHistory(fileName);
+  }
+
+  static saveHistory(guid) {
+    const fs = require("fs");
+    // history
+    const fileName = "filehistory";
+    const content = guid.toString();
+    console.log(content);
+    fs.writeFile(__dirname + "/db/history/" + fileName, content, (err) => {
       if (err) {
         console.log(err);
         return;
