079195a93a5e5497ea88721b099faf815b8ea366
Uonai <cs.mccaleb@gmail.com>
2020-05-15 18:12:17 -0500

    dropdowns in modals

--- client/js/debtModal.js
+++ client/js/debtModal.js
@@ -9,23 +9,20 @@ export default class DebtModal {
         <div class="form-group">
           <input type="text" id="form-calculator-title" class="form-control" placeholder="Title" />
         </div>
-        <div class="form-group">
-          <input type="text" id="form-calculator-amount" class="form-control" placeholder="Amount" />
-        </div>
         <select id="Category">
-          <option value="1">Secured</option>
-          <option value="2">Unsecured</option>
-          <option value="3">Revolving</option>
-          <option value="4">Non-revolving</option>
-        </select>
-
-        <div class="form-group">
-          <input type="text" id="form-calculator-interest" class="form-control" placeholder="Interest" />
-        </div>
-
+        <option value="1">Secured</option>
+        <option value="2">Unsecured</option>
+        <option value="3">Revolving</option>
+        <option value="4">Non-revolving</option>
+      </select>
         <div class="form-group">
           <input type="text" id="form-calculator-monthly-payment" class="form-control" placeholder="Monthly Payment" />
-        </div>`;
+        </div>
+        <div class="form-group">
+        <input type="text" id="form-calculator-amount" class="form-control" placeholder="Remaining Months" />
+      </div>
+        
+        `;
     const formContainer = document.querySelector("#modal-calculator-footer");
     const parent = document.querySelector("#modal-calculator-form");
     parent.insertBefore(div, formContainer);
